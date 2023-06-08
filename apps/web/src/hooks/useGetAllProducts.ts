81b912f892cbc2d63ab2a9b35bb9ba8f114c6ccc
divyansh0908nl <divyansha@northladder.com>
2023-06-08 13:37:00 +0530

    reafactor! :  use react query hook from `@jadoo/services`
    - implemented the similar logic using `@jadoo/services` dependency instead of `@tanstack/react-query`
    - Refactor the codebase to conform to the lint configuration.

--- apps/web/src/hooks/useGetAllProducts.ts
+++ apps/web/src/hooks/useGetAllProducts.ts
@@ -1,15 +1,12 @@
-import { useQuery } from "@tanstack/react-query";
-import { getAllProducts } from "../API/products";
+import { useQuery } from '@jadoo/services';
+import { getAllProducts } from '../API/products';
 
-
-
-export const useGetAllProducts = (onSuccess=()=>{}, onError=()=>{}) => {
-    const { data,error,isError, isLoading } = useQuery(
-        { queryKey: ["allProducts"], queryFn: getAllProducts,
-            onSuccess,
-            onError
-        } 
-
-    );
-    return { data, error, isError, isLoading };
-    }
+export const useGetAllProducts = (onSuccess = () => {}, onError = () => {}) => {
+  const { data, error, isError, isLoading } = useQuery({
+    queryKey: ['allProducts'],
+    queryFn: getAllProducts,
+    onSuccess,
+    onError,
+  });
+  return { data, error, isError, isLoading };
+};
