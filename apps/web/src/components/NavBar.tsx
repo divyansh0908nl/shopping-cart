106379b58bc00621d6a749de8d3f25e97e1e27a6
divyansh0908nl <divyansha@northladder.com>
2023-06-15 17:04:08 +0530

    refactor: Refactor code to adhere with readability and lint
    - Updated the code in the ProductCard.tsx file by refactoring the container class for improved readability and maintainability.

--- apps/web/src/components/NavBar.tsx
+++ apps/web/src/components/NavBar.tsx
@@ -8,22 +8,35 @@ import Image from 'next/image';
 export function NavBar() {
   return (
     <header className="body-font text-gray-600">
-      <div className="container mx-auto flex flex-col flex-wrap items-center p-5 md:flex-row">
+      <div
+        className="
+          container mx-auto flex flex-col flex-wrap items-center p-5
+          md:flex-row
+        "
+      >
         <Link href="/">
           <Image src="/logo.png" alt="logo" width={100} height={100} />
         </Link>
-        <nav className="flex flex-wrap items-center justify-center text-base md:ml-auto">
+        <nav
+          className="
+            flex flex-wrap items-center justify-center text-base md:ml-auto
+          "
+        >
           <Link
             href="/addproducts"
-            className="mr-5 rounded-md bg-green-200 px-2 py-1 hover:bg-green-400
-                      hover:text-gray-900"
+            className="
+              mr-5 rounded-md bg-green-200 px-2 py-1 hover:bg-green-400
+              hover:text-gray-900
+            "
           >
             Add Product
           </Link>
           <Link
             href="/products"
-            className="mr-5 rounded-md bg-red-200 px-2 py-1 hover:bg-red-400
-                      hover:text-gray-900"
+            className="
+              mr-5 rounded-md bg-red-200 px-2 py-1 hover:bg-red-400
+              hover:text-gray-900
+            "
           >
             All Products
           </Link>
