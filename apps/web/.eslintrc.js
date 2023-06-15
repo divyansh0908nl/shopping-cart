c86e611d1c2b82e38b41320b3b6b1c79ff9496a6
divyansh0908nl <divyansha@northladder.com>
2023-06-15 12:24:51 +0530

    chore: Remove testing-library plugin and extnsn from `web/.eslintrc.js`
    - Removed the `testing-library` plugin and the `plugin:testing-library/react` extension from the `.eslintrc` file.
    - These configurations were no longer needed in the project.

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -3,7 +3,6 @@ const path = require('path');
 module.exports = {
   root: true,
   extends: ['@jadoo/eslint-config-custom'],
-  plugins: ['testing-library'],
   parserOptions: {
     project: path.join(__dirname, 'tsconfig.json'),
   },
@@ -11,7 +10,6 @@ module.exports = {
     // Only uses Testing Library lint rules in test files
     {
       files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
-      extends: ['plugin:testing-library/react'],
     },
   ],
   ignorePatterns: ['charts/*'],
