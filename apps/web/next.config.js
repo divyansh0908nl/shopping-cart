10def13ba0ac9310fed82f069cf413d8d680c33f
divyansh0908nl <divyansha@northladder.com>
2023-06-15 12:12:38 +0530

    perf: Add remote image configuration to next.config.js
    - Add configuration for remote images in the `next.config.js` file.
    - The `images` property now includes a `remotePatterns` array with a single pattern for images hosted on `http://192.168.31.44:5000/uploads/`.
    - This allows Next.js to fetch and optimize images from the specified remote location.

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,14 @@
 module.exports = {
   reactStrictMode: true,
   transpilePackages: ['@jadoo/design-system', '@jadoo/services', '@jadoo/apis'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '192.168.31.44',
+        port: '5000',
+        pathname: '/uploads/**',
+      },
+    ],
+  },
 };
