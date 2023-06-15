e09e213be09687edfa7317631f5b5d0d1716ced7
divyansh0908nl <divyansha@northladder.com>
2023-06-15 11:48:00 +0530

    removal: remove unnecessary files
    ## `apps/pages`
    
    - remove `addproducts.tsx` as another package `addproduct.tsx` exist.
    
    ## `packages/service`
    - remove `services/src/hooks` and `services/src/components` as the files are already made available inside `services/src/react-query`

---
+++ apps/web/src/pages/addproduct.tsx
@@ -0,0 +1,16 @@
+import { NavBar } from '../components/NavBar';
+
+/*
+ * ---------------------------------------------------------------------------
+ * page for adding new products
+ * will be completed in next PR
+ */
+function addproduct() {
+  return (
+    <>
+      <NavBar />
+      <h1>Add Product</h1>
+    </>
+  );
+}
+export default addproduct;
--- packages/services/src/hooks/ReactQueryHooks.tsx
+++
@@ -1,4 +0,0 @@
-export { useQuery } from '@tanstack/react-query';
-export { useMutation } from '@tanstack/react-query';
-export { useQueryClient } from '@tanstack/react-query';
-export { useInfiniteQuery } from '@tanstack/react-query';
